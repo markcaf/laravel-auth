59c2284ca445d3cbfe089fbaf98df8705620cf1a
markcaf <marcocafassog@gmail.com>
2022-09-19 18:40:31 +0200

    feat: add the seeder for the posts table

---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 50; $i++) { 
+            $newPost = new Post();
+            $newPost->title = $faker->realText(35);
+            $newPost->author = $faker->userName();
+            $newPost->post_date = $faker->dateTimeThisYear();
+            $newPost->post_content = $faker->paragraphs(5, true);
+
+            $newPost->save();
+        }
+    }
+}
