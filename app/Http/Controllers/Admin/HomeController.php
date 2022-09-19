cd3acf81b2a89ab419d039cb5aedd081564a3e73
markcaf <marcocafassog@gmail.com>
2022-09-19 18:03:22 +0200

    test: dd of currently authenticated user and id

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -24,6 +25,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        // Get the currently authenticated user
+        $user = Auth::user();
+
+        // Get the currently authenticated user's ID
+        $id = Auth::id();
+
+        dd($user, $id);
+
+        return view('admin.home', ['id' => $id, 'user' => $user]);
     }
 }
