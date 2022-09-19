4619fe7aa6ff2700c72104809ccc4fd9989f93b2
markcaf <marcocafassog@gmail.com>
2022-09-19 18:15:12 +0200

    refactor: change welcome view into guest.welcome

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
@@ -25,4 +25,5 @@ Route::middleware('auth')
     ->prefix('admin')
     ->group(function () {
         Route::get('/', 'HomeController@index')->name('home'); 
+        Route::resource('/posts', 'PostController');
     });
