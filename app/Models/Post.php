582730856b471302b5ede5a526ae1ca7477f6fb6
markcaf <marcocafassog@gmail.com>
2022-09-20 13:14:17 +0200

    feat(Post): add fillable into the post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = [
+        'slug',
+        'title',
+        'post_content',
+        'post_image',
+        'post_date',
+        'author',
+    ];
 }
