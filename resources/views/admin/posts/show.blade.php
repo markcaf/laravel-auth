65fedbfe99dad0a148bfbd49d4b63c4ce28d5d77
markcaf <marcocafassog@gmail.com>
2022-09-21 17:41:29 +0200

    feat(show): add edit and delete buttons

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -13,7 +13,7 @@
                 "{{ session('edited') }}" has been successfully edited.
             </div>
         @endif
-        
+
         <h2 class="my-3">{{ $post->title }}</h2>
 
         <div class="row">
@@ -24,6 +24,22 @@
                 <p>{{ $post->post_content }}</p>
                 <p><strong>Author:</strong> {{ $post->author }}</p>
                 <p><strong>Post Date:</strong> {{ $post->post_date }}</p>
+            </div>
+            <div class="col-12 mt-5 d-flex justify-content-center align-items-center">
+                <div>
+                    <a href="{{ route('admin.posts.edit', $post->slug) }}" class="btn btn btn-success mx-2">
+                        Edit
+                    </a>
+                </div>
+
+                <div>
+                    <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST" class="form-post-delete"
+                        data-post-name="{{ $post->title }}">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn btn-danger mx-2">Delete</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
