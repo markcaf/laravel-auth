31a2d78c6b327d0a3d80bc1058a57c3f602456af
markcaf <marcocafassog@gmail.com>
2022-09-21 17:27:40 +0200

    feat(show): add session msg for create and edit

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -1,16 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container-lg">
-        <h2 class="my-3">{{$post->title}}</h2>
+    <div class="container">
+        @if (session('created'))
+            <div class="col-12 alert alert-success">
+                "{{ session('created') }}" has been successfully created.
+            </div>
+        @endif
+
+        @if (session('edited'))
+            <div class="col-12 alert alert-success">
+                "{{ session('edited') }}" has been successfully edited.
+            </div>
+        @endif
+        
+        <h2 class="my-3">{{ $post->title }}</h2>
+
         <div class="row">
             <div class="col-4">
-                <img class="img-fluid" src="{{$post->post_image}}" alt="{{$post->title}}">
+                <img class="img-fluid" src="{{ $post->post_image }}" alt="{{ $post->title }}">
             </div>
             <div class="col-8">
-                <p>{{$post->post_content}}</p>
-                <p><strong>Author:</strong> {{$post->author}}</p>
-                <p><strong>Post Date:</strong> {{$post->post_date}}</p>
+                <p>{{ $post->post_content }}</p>
+                <p><strong>Author:</strong> {{ $post->author }}</p>
+                <p><strong>Post Date:</strong> {{ $post->post_date }}</p>
             </div>
         </div>
     </div>
